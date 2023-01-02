a4198464ca0c6f2100dfad905759c1bbe09a5d4e
mircea95 <psihhh95@gmail.com>
2023-01-02 16:13:48 +0200

    flask configur

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template("info.html")
+
+@app.route('/contacts/')
+def contacts():
+   return render_template("contacts.html")
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=80, debug=True)
