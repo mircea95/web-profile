2c88d1a74ad0846e421c313598375831b522ded9
mircea95 <36150151+mircea95@users.noreply.github.com>
2023-01-02 18:29:56 +0200

    app to application

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
-app = Flask(__name__)
 
-@app.route('/')
+# ------------------------------------------------
+# Program by Mircea Bularga
+#
+#
+# Version      Date           Info
+# 1.0          02-01-2023     Initial Version
+#
+# ----------------------------------------------
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
+@application.route('/')
 def index():
    return render_template("info.html")
 
-@app.route('/contacts/')
+@application.route('/contacts/')
 def contacts():
    return render_template("contacts.html")
 
 if __name__ == '__main__':
-   app.run()
+   application.debug = True
+   application.run()
